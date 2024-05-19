6e5a7516e91761b474251d2d6d874dec09332d2e
Nockolay23 <serebrenicof@gmail.com>
2024-05-19 14:13:39 +0300

    Создал проект в qt

---
+++ TestPage/build/6-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "NMake Makefiles JOM" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/sereb/Documents/GitHub/TestPageQT/TestPage/build/6-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/msys64/mingw64/qt6-static/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/msys64/mingw64/qt6-static" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/msys64/mingw64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
